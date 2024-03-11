8d059a9673b3a86df3e2f57b460e2f8ac0366796
namaek_2 <science4588@gmail.com>
2024-03-11 10:59:29 +0900

    nbo featured

--- add-nbo.c
+++ add-nbo.c
@@ -8,33 +8,36 @@ void add_nbo(char* argv[]) {
     FILE* file1= fopen(argv[1], "r");
     FILE* file2= fopen(argv[2], "r");
 
-    if (file1 == NULL) {
-        printf("파일 열기 실패\n");
-        return;
-    } else if (file2 == NULL) {
+    if (file1 == NULL || file2 == NULL) {
         printf("파일 열기 실패\n");
         return;
     }
 
-    uint32_t result = 0;
-    uint32_t f1;
-    uint32_t f2;
-    size_t f_size;
+    uint32_t result, f1, f2;
+    uint32_t t1, t2, t3, t4;
 
-    while ((f_size = fread(&f1, sizeof(uint32_t), 1, file1)) > 0) {
-        printf("%08X ", f1);
-    }
+    fread(&f1, sizeof(uint32_t), 1, file1);
+    fread(&f2, sizeof(uint32_t), 1, file2);
 
-    result += f1;
+    return;
+    t1 = (f1 & 0xFF000000) >> 24;
+    t2 = (f1 & 0x00FF0000) >> 8;
+    t3 = (f1 & 0x0000FF00) << 8;
+    t4 = (f1 & 0x000000FF) << 24;
 
-    while ((f_size = fread(&f2, sizeof(uint32_t), 1, file2)) > 0) {
-        printf("%08X ", f2);
-    }
+    f1 = t1|t2|t3|t4;
 
-    result += f2;
+    t1 = (f2 & 0xFF000000) >> 24;
+    t2 = (f2 & 0x00FF0000) >> 8;
+    t3 = (f2 & 0x0000FF00) << 8;
+    t4 = (f2 & 0x000000FF) << 24;
 
+    f2 = t1|t2|t3|t4;
+
+    result = f1 + f2;
+
+    printf("%d(0x%x) + %d(0x%x) = %d(0x%x)\n", f1, f1, f2, f2, result, result);
 
     fclose(file1);
     fclose(file2);
-    return;
 }
